872e6410bed265a17fb0217f872162b16a99310d
Loc <hvloc@apcs.vn>
2012-05-15 13:58:10 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '31ebdc79dba7cd7ac757f77da4434947da58970c7d04caaa22c80f21fa57027043ee8c164135ea309ae75f40a885927f3d6063512fcf3484c8b6ee2c82e1e2d5'
